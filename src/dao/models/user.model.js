879ffeba88034783d71ffd7694158e8049211de8
suecollarte <suecollarte@gmail.com>
2023-07-08 21:08:01 -0400

    Delete user.model.js

--- src/dao/models/user.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-export default mongoose.model('Users', mongoose.Schema({
-    first_name: { type: String, index: true },
-    last_name: String,
-    email: String,
-    gender: String
-}))
