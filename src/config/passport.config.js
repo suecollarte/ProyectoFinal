8070995fde24c63f0bb308deb3f86702a2e24aab
suecollarte <suecollarte@gmail.com>
2023-08-01 18:57:42 -0400

    revision Estrategias

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,24 +1,16 @@
 import passport from "passport";
-import jwt from 'passport-jwt'
+import GitHubStartegy from 'passport-github2'
 
-const JWTStrategy = jwt.Strategy;
-const ExtractJWT = jwt.ExtractJwt;
-const cookieExtractor = req => {
-    const token= (req && req.signedCookies) ? req.signedCookies['mysecretjwt'] : null
-    return token
-}
+
+
 const inializePassport = () => {
 
-       passport.use('jwt',new JWTStrategy({
-        jwtFromRequest: ExtractJWT.fromExtractors([cookieExtractor]),
-        secretOrKey:'secret'
-       }, async (jwr_payload,done) =>{
-        try {
-            return done(null, jwr_payload)
-        }catch(err){
-            return done(err)
-        }
-       }))
-    
+       passport.serializeUser((user,done) =>{
+        done(null,user._id)
+       })
+       passport.deserializeUser(async (id, done) =>{
+        let user =await userService.findById(id)
+        done(null,user);
+       })
 }
 export default inializePassport
